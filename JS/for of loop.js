f70bd741f7b99ae97d7d827e6258ef6d14cf772a
praveen3112004 <praveen3112004@gmail.com>
2025-09-27 22:44:44 +0530

    for of loop program

---
+++ JS/for of loop.js
@@ -0,0 +1,21 @@
+let orders = [
+  {
+    orderno: "C001",
+    total_amount: 15000,
+    no_Of_Item: 10,
+    type_of_Customer: "Existing",
+  },
+  {
+    orderno: "C002",
+    total_amount: 5000,
+    no_Of_Item: 2,
+    type_of_Customer: "Existing",
+  },
+  {
+    orderno: "C003",
+    total_amount: 20000,
+    no_Of_Item: 4,
+    type_of_Customer: "New",
+  },
+];
+for(ordersData of orders) console.log("Order No:",ordersData.orderno,"--------","Total Amount:", ordersData.total_amount)
